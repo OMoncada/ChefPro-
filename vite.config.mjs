d69191364a459cb9640d6c4999428d1e036a80b3
O'Moncada <oscar.moncada702@gmail.com>
2024-10-25 10:21:14 -0300

    guardar cambios

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+    root: "src/", // Carpeta raíz del proyecto
     build: {
-        outDir: "./dist", // Ajusta esto si quieres que el build esté en otra carpeta
+        outDir: "../dist", // Carpeta de salida para la compilación final
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "index.html"), // Cambiado a la raíz del proyecto
+                main: resolve(__dirname, "src/index.html"),
                 recipes: resolve(__dirname, "src/recipes/recipes.html"),
                 recipeDetails: resolve(__dirname, "src/recipes/recipe-details.html"),
                 myList: resolve(__dirname, "src/list/my-list.html"),
